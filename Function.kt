dd0b7f78d58d15eb950f9a0f03247ed304299ea7
nitinjunejaa <kumar.juneja11@gmail.com>
2022-07-07 23:33:12 +0530

    Updated

--- Function.kt
+++ Function.kt
@@ -1,6 +1,10 @@
 fun main() {                         // The code witten only in main function will be executed as it is the starting point of the function
     functionName()
     println(timeThree(4587))
+    println(timesthree(45))
+    println(timeFour(87))
+    println(timesFour(48))
+
 
 }
 
@@ -8,4 +12,17 @@ fun functionName(): Unit{
     println("This is just a normal print statement")
 }
 
-fun timeThree(x: Int) = x*3
+fun timeThree(x: Int): Int {
+    val result = x*3
+    return result
+}
+
+fun timesthree(y:Int) = y *3
+
+fun timeFour(n:Int): Int{
+    val output = n * 4
+    return output
+}
+
+fun timesFour(j:Int) = j*4
+
