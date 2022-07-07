aa446424e50d5dab9eb09171130a32ed91e9dbc6
nitinjunejaa <kumar.juneja11@gmail.com>
2022-07-07 23:24:54 +0530

    Updated

--- Function.kt
+++ Function.kt
@@ -1,4 +1,4 @@
-fun main() {                         // The code witten only in main function will be executed as it is the starting pint of the function
+fun main() {                         // The code witten only in main function will be executed as it is the starting point of the function
     functionName()
     println(timeThree(4587))
 
